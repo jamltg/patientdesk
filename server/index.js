8dc06b428504f91dca3953887885f57eed06a69f
Jhame Letigio <jamletigio@gmail.com>
2026-04-13 15:27:07 +0800

    Change index.js code and add port

--- server/index.js
+++ server/index.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+const express = require ('express');
+const app = express();
+
+const port = 3000;
+
+app.listen(port,()=>{
+    console.log(`App running on Port ${port}`);
+})
+
+
