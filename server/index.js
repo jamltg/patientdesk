e2cc0133194ffe1f8ca94ad7e812f228fa015323
Jhame Letigio <jamletigio@gmail.com>
2026-04-13 15:33:44 +0800

    add app.js in server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
-const express = require ('express');
-const app = express();
+const app = require('./app');
 
 const port = 3000;
+
 
 app.listen(port,()=>{
     console.log(`App running on Port ${port}`);
